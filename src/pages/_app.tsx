900aa3b08113c92a5627aa71646f455fa0bd17c1
Luiz Felicio <unifelicio@gmail.com>
2020-11-30 08:18:38 -0300

    refactor: adjust boilerplate stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,16 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import GlobalStyles from '@styles/global';
+import GlobalStyles from 'styles/global';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Hypa - NextJS Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <title>Plataforma Sabiá - Blog</title>
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and styled-components"
+          content="O blog da plataforma de tecnologias do semi-árido brasileiro"
         />
       </Head>
       <GlobalStyles />
