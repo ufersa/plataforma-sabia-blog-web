65cd8ab0bbd68373611eb6513401ae800bc5e920
Luiz Felicio <unifelicio@gmail.com>
2020-11-30 08:56:54 -0300

    feat: add global styles and theme file

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,11 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+  *,
+	*::after,
+	*::before {
+		margin: 0;
+		padding: 0;
+		outline: 0;
+		box-sizing: border-box;
+	}
+  
+	*:focus {
+		outline: 0;
+	}
 
   html {
     font-size: 62.5%;
@@ -16,8 +23,25 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
+    -webkit-font-smoothing: antialiased;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
+
+  a {
+    text-decoration: none;
+    transition: all 0.2s ease;
+    :hover {
+      cursor: pointer;
+    }
+	}
+
+	ul {
+		list-style: none;
+	}
+
+	button {
+		cursor: pointer;
+	}
 `;
 
 export default GlobalStyles;
