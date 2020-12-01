a720af34543da1058b32a8fd84be7895cec2620f
Luiz Felicio <unifelicio@gmail.com>
2020-12-01 16:36:32 -0300

    refactor: adjust graphql schema

--- src/types/graphql-schema.ts
+++ src/types/graphql-schema.ts
@@ -1,15 +1,18 @@
 export type Category = {
   id: string | number;
-  created_at: Date;
-  updated_at: Date;
   name: string;
+};
+
+export type Thumbnail = {
+  id: string;
+  url: string;
 };
 
 export type Post = {
   id: string | number;
   title: string;
   subtitle?: string;
-  thumbnail?: { id: string; url: string };
+  thumbnail?: Thumbnail;
   content: string;
   categories: Category[];
   published_at: Date;
