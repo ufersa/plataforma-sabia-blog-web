656fccec09e47ef3f48afea2376f7684fb0bf647
Luiz Felicio <unifelicio@gmail.com>
2020-12-01 14:10:43 -0300

    feat: add service for single post

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,6 +4,11 @@ const apiEndpoint = process.env.NEXT_PUBLIC_GRAPHQL_API;
 
 const client = new GraphQLClient(apiEndpoint!);
 
+/**
+ * Gets all posts
+ *
+ * @returns {array} All posts
+ */
 export const getPosts = async () => {
   const query = gql`
     {
@@ -29,4 +34,36 @@ export const getPosts = async () => {
   const { posts } = await client.request(query);
 
   return posts;
+};
+
+/**
+ * Gets a single post by slug
+ *
+ * @returns {object} Post object
+ */
+export const getPost = async (slug = '') => {
+  const query = gql`
+    query getPost($slug: String!) {
+      posts(where: { slug: $slug }) {
+        id
+        thumbnail {
+          id
+          url
+        }
+        title
+        subtitle
+        content
+        categories {
+          id
+          name
+        }
+        slug
+        published_at
+      }
+    }
+  `;
+
+  const { posts } = await client.request(query, { slug });
+
+  return posts[0];
 };
