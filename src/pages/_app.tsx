970d057f3a76c0095f6c7a1ae7c44e9adc42fcbd
Luiz Felicio <unifelicio@gmail.com>
2020-12-02 17:22:59 -0300

    feat: add nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Router from 'next/router';
 import { ThemeProvider } from 'styled-components';
 
 import Layout from 'components/Layout';
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
+import NProgress from 'nprogress';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function App({ Component, pageProps }: AppProps) {
   return (
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,45 +1,129 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-  *,
-	*::after,
-	*::before {
-		margin: 0;
-		padding: 0;
-		outline: 0;
-		box-sizing: border-box;
-	}
-  
-	*:focus {
-		outline: 0;
-	}
+	${({ theme: { colors } }) => css`
+    *,
+    *::after,
+    *::before {
+      margin: 0;
+      padding: 0;
+      outline: 0;
+      box-sizing: border-box;
+    }
 
-  html {
-    font-size: 62.5%;
-  }
+    *:focus {
+      outline: 0;
+    }
 
-  body {
-    -webkit-font-smoothing: antialiased;
-    font-family: -apple-system, BlinkMacSystemFont, Montserrat, sans-serif;
-		overflow-x: hidden;
-  }
+    html {
+      font-size: 62.5%;
+    }
 
-  a {
-    text-decoration: none;
-    color: inherit;
-    transition: all 0.2s ease;
-    :hover {
+    body {
+      -webkit-font-smoothing: antialiased;
+      font-family: -apple-system, BlinkMacSystemFont, Montserrat, sans-serif;
+      overflow-x: hidden;
+    }
+
+    a {
+      text-decoration: none;
+      color: inherit;
+      transition: all 0.2s ease;
+      :hover {
+        cursor: pointer;
+      }
+    }
+
+    ul {
+      list-style: none;
+    }
+
+    button {
       cursor: pointer;
     }
-	}
 
-	ul {
-		list-style: none;
-	}
+    /* NProgress Styles */
+    #nprogress {
+      pointer-events: none;
+    }
 
-	button {
-		cursor: pointer;
-	}
+    #nprogress .bar {
+      background: ${colors.darkGreen};
+
+      position: fixed;
+      z-index: 1100;
+      top: 0;
+      left: 0;
+
+      width: 100%;
+      height: 2px;
+    }
+
+    /* Fancy blur effect */
+    #nprogress .peg {
+      display: block;
+      position: absolute;
+      right: 0px;
+      width: 100px;
+      height: 100%;
+      box-shadow: 0 0 10px #29d, 0 0 5px #29d;
+      opacity: 1;
+
+      -webkit-transform: rotate(3deg) translate(0px, -4px);
+      -ms-transform: rotate(3deg) translate(0px, -4px);
+      transform: rotate(3deg) translate(0px, -4px);
+    }
+
+    /* Remove these to get rid of the spinner */
+    #nprogress .spinner {
+      display: block;
+      position: fixed;
+      z-index: 1100;
+      top: 15px;
+      right: 15px;
+    }
+
+    #nprogress .spinner-icon {
+      width: 18px;
+      height: 18px;
+      box-sizing: border-box;
+
+      border: solid 2px transparent;
+      border-top-color: ${colors.darkGreen};
+      border-left-color: ${colors.darkGreen};
+      border-radius: 50%;
+
+      -webkit-animation: nprogress-spinner 400ms linear infinite;
+      animation: nprogress-spinner 400ms linear infinite;
+    }
+
+    .nprogress-custom-parent {
+      overflow: hidden;
+      position: relative;
+    }
+
+    .nprogress-custom-parent #nprogress .spinner,
+    .nprogress-custom-parent #nprogress .bar {
+      position: absolute;
+    }
+
+    @-webkit-keyframes nprogress-spinner {
+      0% {
+        -webkit-transform: rotate(0deg);
+      }
+      100% {
+        -webkit-transform: rotate(360deg);
+      }
+    }
+    @keyframes nprogress-spinner {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  `}
 `;
 
 export default GlobalStyles;
